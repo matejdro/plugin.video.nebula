1d181ca0a2889dd38315d87f30367bc1125684db
Matej Drobnič <matejdro@gmail.com>
2020-02-29 19:50:52 +0100

    display list of categories

--- plugin.video.nebula/main.py
+++ plugin.video.nebula/main.py
@@ -2,6 +2,7 @@ import sys
 from urllib import urlencode
 from urlparse import parse_qsl
 import xbmcgui
+import xbmc
 import xbmcplugin
 import requests
 import xbmcaddon
@@ -13,8 +14,44 @@ _url = sys.argv[0]
 # Get the plugin handle as an integer number.
 _handle = int(sys.argv[1])
 
+def get_url(**kwargs):
+    """
+    Create a URL for calling the plugin recursively from the given set of keyword arguments.
+
+    :param kwargs: "argument=value" pairs
+    :type kwargs: dict
+    :return: plugin call URL
+    :rtype: str
+    """
+    return '{0}?{1}'.format(_url, urlencode(kwargs))
+
+def display_global_category_list():
+    categories = api.get_categories()
+
+    xbmcplugin.setPluginCategory(_handle, "Categories")
+    xbmcplugin.setContent(_handle, "videos")
+
+    for category in categories:
+        title = category[0]
+        category_id = categories[1]
+
+        list_item = xbmcgui.ListItem(label=title)
+        list_item.setInfo("video", {"mediatype": "video"})
+        list_item.setProperty("IsPlayable", "false")
+
+        url = get_url(action='category', id=category_id)
+
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, True)
+
+    xbmcplugin.endOfDirectory(_handle)
+        
+
+
 def router():
-    api.login()
+    if storage.get_nebula_token() is None:
+        api.login()
+    
+    display_global_category_list()
 
 if __name__ == '__main__':
     try:
--- plugin.video.nebula/nebulalib/api.py
+++ plugin.video.nebula/nebulalib/api.py
@@ -1,6 +1,8 @@
 import storage
 import requests
 import xbmc
+import re
+import json
 
 class InvalidCredentials(Exception):
     pass
@@ -18,3 +20,19 @@ def login():
     result_json = user_data.json()
 
     storage.set_nebula_token(result_json["key"])
+
+def fetch_channel_list():
+    html_result = requests.get("https://watchnebula.com").text
+
+    initial_state = re.search('<script id="initial-app-state" type="application/json">((.|\n)*?)</script>', html_result).group(1)
+    return json.loads(initial_state)
+
+def get_channel_list():
+    return fetch_channel_list()
+
+def get_categories():
+    categories = [(k,v) for k,v in get_channel_list()["categories"]["byTitle"].items() if k[0] != "_"]
+    categories.sort(key=lambda a: a[0])
+
+    return categories
+    
